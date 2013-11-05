67ab753dfe2692336a223937b937abc9d0b6785d
iionly <iionly@gmx.de>
2013-11-05 19:37:56 +0100

    Initial release for Elgg 1.9; see README.txt

---
+++ unvalidatedemailchange/languages/en.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Unvalidatedemailchange plugin
+ * iionly
+ * Contact: iionly@gmx.de
+ *
+ * The Unvalidatedemailchange is based on:
+ *
+ * @package simpleusermanagement
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Pjotr Savitski
+ * @copyright (C) Pjotr Savitski
+ * @link http://code.google.com/p/simpleusermanagement/
+ **/
+
+return array(
+'unvalidatedemailchange:no_user_guid_provided' => 'User-GUID not provided.',
+'unvalidatedemailchange:user_email_changed_to' => 'Email address of user %s changed to %s.',
+'unvalidatedemailchange:email_address_invalid' => 'Provided email address %s is invalid.',
+'unvalidatedemailchange:new_user_email' => 'Enter the new email address for %s: ',
+'unvalidatedemailchange:change_email' => 'Change email'
+);
---
+++ unvalidatedemailchange/start.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Unvalidatedemailchange plugin
+ * (c) iionly 2012-2013
+ * Contact: iionly@gmx.de
+ * Website: https://github.com/iionly
+ * License: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ *
+ */
+
+elgg_register_event_handler('init', 'system', 'unvalidatedemailchange_init');
+
+function unvalidatedemailchange_init() {
+    elgg_register_action('unvalidatedemailchange/change_user_email', elgg_get_plugins_path() . "unvalidatedemailchange/actions/change_user_email.php", 'admin');
+    elgg_register_ajax_view('unvalidatedemailchange/change_email');
+}
